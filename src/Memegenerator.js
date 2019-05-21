b96cab69dc8bb7ffa49d8d3c56da1e6c183acb8b
Antonijo <antonijodev@gmail.com>
2019-05-21 15:15:13 +0100

    State setup and fetching meme API

--- src/Memegenerator.js
+++ src/Memegenerator.js
@@ -4,8 +4,21 @@ class MemeGenerator extends Component {
   constructor() {
     super();
     this.state = {
-
+      topText: "",
+      bottomText: "",
+      randomImage: "http://i.imgflip.com/1bij.jpg",
+      allMemeImages: []
     }
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+          const {memes} = response.data
+          console.log(memes[0]);
+          this.setState({ allMemeImages: memes })
+        })
   }
 
   render() {
